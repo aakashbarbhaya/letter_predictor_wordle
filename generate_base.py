fb17c8ae53b13fc43488a98b5a64da3a92bc539b
Aakash Barbhaya <abarbhay@amazon.com>
2022-01-27 19:36:08 -0800

    phase 1

---
+++ generate_base.py
@@ -0,0 +1,86 @@
+import json
+
+all_word_file_path = 'files/all_words.txt'
+find_words_for_length_path = 'files/find_words_for_length.txt'
+unique_words_for_length_path = 'files/unique_words_for_length_path.txt'
+weighted_character_dict_path = 'files/weighted_character_dict.txt'
+weighted_words_path = 'files/weighted_words_path.txt'
+
+
+def find_words_for_length(length):
+    read_file = open(all_word_file_path, 'r')
+    write_file = open(find_words_for_length_path, 'w')
+    words = read_file.readlines()
+    for word in words:
+        if len(word.strip()) == length:
+            write_file.writelines(word.strip() + '\n')
+    read_file.close()
+    write_file.close()
+
+
+def create_weighted_character_dict():
+    character_dict = dict()
+    read_file = open(find_words_for_length_path, 'r')
+    write_file = open(weighted_character_dict_path, 'w')
+    words = read_file.readlines()
+
+    for word in words:
+        split_list = list(word.strip())
+        for character in split_list:
+            if character in character_dict:
+                character_dict[character] = character_dict[character] + 1
+            else:
+                character_dict[character] = 1
+    sorted_character_dict = dict(sorted(character_dict.items(), key=lambda item: item[1], reverse=True))
+    sum = 0
+    for character_key, character_value in sorted_character_dict.items():
+        sum = sum + character_value
+
+    weighted_character_dict = dict()
+    for character_key, character_value in sorted_character_dict.items():
+        weighted_character_dict[character_key] = (character_value / sum) * 100
+
+    write_file.write(json.dumps(weighted_character_dict, indent=4))
+    read_file.close()
+    write_file.close()
+
+
+def create_list_of_words_with_unique_characters():
+    read_file = open(find_words_for_length_path, 'r')
+    write_file = open(unique_words_for_length_path, 'w')
+    words = read_file.readlines()
+    for word in words:
+        if len(set(word)) == len(word):
+            write_file.writelines(word)
+    read_file.close()
+    write_file.close()
+
+
+def create_weighted_word_list():
+    character_weight_json_file = open(weighted_character_dict_path, 'r')
+    character_weight_json = json.load(character_weight_json_file)
+
+    read_file = open(unique_words_for_length_path, 'r')
+    write_file = open(weighted_words_path, 'w')
+
+    words = read_file.readlines()
+    weighted_word_dict = dict()
+
+    for word in words:
+        split_word = list(word.strip())
+        sum = 0
+        for character in split_word:
+            sum = sum + character_weight_json[character]
+        weighted_word_dict[word.strip()] = sum
+
+    sorted_weighted_word_dict = dict(sorted(weighted_word_dict.items(), key=lambda item: item[1], reverse=True))
+    write_file.write(json.dumps(sorted_weighted_word_dict, indent=4))
+    read_file.close()
+    write_file.close()
+
+
+def generate_base(length):
+    find_words_for_length(length)
+    create_weighted_character_dict()
+    create_list_of_words_with_unique_characters()
+    create_weighted_word_list()
---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+from generate_base import generate_base
+
+weighted_words_path = 'files/weighted_words_path.txt'
+
+
+def find_best_combination():
+    weighted_word_json_file = open(weighted_words_path, 'r')
+    weighted_words_json = json.load(weighted_word_json_file)
+
+    combination_of_words = list()
+    for word in weighted_words_json:
+        if len(combination_of_words) == 0:
+            combination_of_words.append(word.strip())
+
+        accounted_characters = list()
+        for combination_word in combination_of_words:
+            split_combination_word = list(combination_word)
+            accounted_characters.extend(split_combination_word)
+
+        split_word = list(word)
+        if len(set(split_word).intersection(set(accounted_characters))) == 0:
+            combination_of_words.append(word.strip())
+
+        elif len(combination_of_words) == 2:
+            if len(set(split_word).intersection(set(accounted_characters))) <= 1:
+                combination_of_words.append(word.strip())
+
+        if len(combination_of_words) == 3:
+            break
+
+    print(combination_of_words)
+    weighted_word_json_file.close()
+
+
+def main(length):
+    generate_base(length)
+    find_best_combination()
+
+
+if __name__ == '__main__':
+    main(5)
